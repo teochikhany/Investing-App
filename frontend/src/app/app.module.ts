2d8ad59c78064a2dcefe2a3f9e2f2dfaf68ff44f
Teo Chikhany <47039131+teochikhany@users.noreply.github.com>
2021-12-11 14:55:13 +0200

    moved root app to folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MatTableModule } from '@angular/material/table'
 import { MatButtonModule } from '@angular/material/button';
