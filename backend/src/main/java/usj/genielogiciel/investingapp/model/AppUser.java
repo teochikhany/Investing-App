e3e7e2a5a6d85114c8c1175a4684a0a51e339611
Teo Chikhany <47039131+teochikhany@users.noreply.github.com>
2021-12-14 17:39:45 +0200

    added user and role models

---
+++ backend/src/main/java/usj/genielogiciel/investingapp/model/AppUser.java
@@ -0,0 +1,27 @@
+package usj.genielogiciel.investingapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+// @Table
+@Data                   // getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
---
+++ backend/src/main/java/usj/genielogiciel/investingapp/model/Role.java
@@ -0,0 +1,23 @@
+package usj.genielogiciel.investingapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+// @Table
+@Data                   // getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+}
