659607d17cee07b3916829d0615ce0b52e73ade9
Teo Chikhany <47039131+teochikhany@users.noreply.github.com>
2021-12-16 17:32:44 +0200

    redirect after successful login

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -3,34 +3,37 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { loginInfo } from 'src/models/loginInfo';
 import { NotificationService } from './notification.service';
 import { tokens } from 'src/models/tokens';
+import { Router } from '@angular/router';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
     private readonly loginUrl = 'http://localhost:8080/api/v1/login';
     private static accessToken = ""
 
-    constructor(private http: HttpClient, private notification: NotificationService) { }
+    constructor(private http: HttpClient, private notification: NotificationService, private router: Router) { }
 
-    static getAccessToken() : string
-    {
+    static getAccessToken(): string {
         return this.accessToken;
     }
 
-    login(user: loginInfo)
-    {
+    login(user: loginInfo) {
         const form = new FormData;
         form.append('username', user.username);
         form.append('password', user.password);
 
-        var request = this.http.post<tokens>(this.loginUrl, form, { observe: 'response'})
+        var request = this.http.post<tokens>(this.loginUrl, form, { observe: 'response' })
 
         request.subscribe({
-            next: (value: HttpResponse<tokens>) => { UserService.accessToken = value.body?.access_token!! },
+            next: (value: HttpResponse<tokens>) => { UserService.accessToken = value.body?.access_token!!; this.changeRoute() },
             error: err => { this.notification.showError(err); }
         });
+    }
+
+    private changeRoute() {
+        this.router.navigate(['/home']);
     }
 }
